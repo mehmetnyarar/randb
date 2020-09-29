9c9223d291856aff1b99bdb937c0c8e2020bfbcb
Mehmet Niyazi Yarar <mehmetnyarar@gmail.com>
2020-09-30 03:46:43 +0600

    test: decrease coverage thresholds to 25% in all packages

--- packages/shared-ui/jest.config.js
+++ packages/shared-ui/jest.config.js
@@ -24,10 +24,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 50,
-      branches: 50,
-      functions: 50,
-      lines: 50
+      statements: 25,
+      branches: 25,
+      functions: 25,
+      lines: 25
     }
   }
 }
